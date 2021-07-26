a8366ac366765ac669dc1c07d977fd1265fd2430
myl7 <myl@myl.moe>
2021-07-26 23:25:32 +0800

    Fix fid stringify err

--- src/fid.rs
+++ src/fid.rs
@@ -15,7 +15,12 @@ impl Fid {
 
 impl Display for Fid {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(self.to_string().as_str())
+        f.write_str(
+            self.id
+                .map(|i| i.to_string())
+                .unwrap_or("0".to_owned())
+                .as_str(),
+        )
     }
 }
 
