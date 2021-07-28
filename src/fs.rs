4c6f073f197fd9afaa7ae1a1532fdf166aea26be
myl7 <myl@myl.moe>
2021-07-29 03:58:06 +0800

    Add dummy attr impl
    Now compatible with ls

--- src/fs.rs
+++ src/fs.rs
@@ -1,11 +1,15 @@
 use crate::client::RecClient;
 use crate::fid::Fid;
 use crate::fidmap::FidMap;
-use fuse_mt::{DirectoryEntry, FilesystemMT, RequestInfo, ResultEntry, ResultOpen, ResultReaddir};
+use fuse_mt::{
+    DirectoryEntry, FileAttr, FileType, FilesystemMT, RequestInfo, ResultEntry, ResultOpen,
+    ResultReaddir,
+};
 use std::borrow::{Borrow, BorrowMut};
 use std::ffi::OsString;
 use std::path::Path;
 use std::sync::{Arc, RwLock};
+use time::Timespec;
 
 pub struct RecFs {
     client: RecClient,
@@ -23,11 +27,33 @@ impl RecFs {
 
 impl FilesystemMT for RecFs {
     fn getattr(&self, _req: RequestInfo, path: &Path, fh: Option<u64>) -> ResultEntry {
-        let fid = if let Some(fh) = fh {
-            self.get_fid(fh)?
+        let (fid, parent_fid) = if let Some(fh) = fh {
+            self.get_fid_with_parent(fh)?
         } else {
             self.req_fid(path)?
         };
+        let mut attr = FileAttr {
+            size: 1,
+            blocks: 1,
+            atime: Timespec::new(0, 0),
+            mtime: Timespec::new(0, 0),
+            ctime: Timespec::new(0, 0),
+            crtime: Timespec::new(0, 0),
+            kind: FileType::Directory,
+            perm: (libc::S_IRUSR & libc::S_IWUSR) as u16,
+            nlink: 0,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            flags: 0,
+        };
+        if let Some(_) = parent_fid {
+            if let Err(_) = self.client.list(fid) {
+                attr.kind = FileType::RegularFile;
+                return Ok((Timespec::new(1, 0), attr));
+            }
+        }
+        Ok((Timespec::new(1, 0), attr))
     }
 
     fn opendir(&self, _req: RequestInfo, path: &Path, _flags: u32) -> ResultOpen {
@@ -59,7 +85,7 @@ impl RecFs {
     fn req_fid(&self, path: &Path) -> Result<(Fid, Option<Fid>), libc::c_int> {
         let mut parent_fid = None;
         let mut fid = Fid::root();
-        for c in path.canonicalize().unwrap().components().skip(1) {
+        for c in path.components().skip(1) {
             let items = self.client.list(fid).map_err(|_| libc::ENOENT)?;
             let s = c.as_os_str().to_string_lossy();
             match items.iter().find(|i| i.name == s) {
@@ -81,5 +107,12 @@ impl RecFs {
             .borrow()
             .get(fh)
             .ok_or(libc::EBADF)?)
+    }
+
+    fn get_fid_with_parent(&self, fh: u64) -> Result<(Fid, Option<Fid>), libc::c_int> {
+        let map = self.fid_map.read().unwrap();
+        let fid = map.borrow().get(fh).ok_or(libc::EBADF)?;
+        let parent_fid = map.borrow().get_parent(fid).unwrap();
+        Ok((fid, parent_fid))
     }
 }
